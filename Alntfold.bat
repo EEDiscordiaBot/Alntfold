141dba0672396f61d48cf5f25f87cbfb18947ef1
EEDiscordiaBot <72722387+EEDiscordiaBot@users.noreply.github.com>
2020-10-22 14:56:30 -0700

    Create Alntfold.bat

---
+++ Alntfold.bat
@@ -0,0 +1,4 @@
+@Echo OFF
+
+Powershell .\luvit Alntfold-main\Alntfold-main\Alntfold.lua
+rem This bat file belongs in the same place as your dependnecies. You should change the above path file to reflect this.
